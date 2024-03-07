67ebb8eae70719f514c5711d404866a131c4b66a
Oskar <assawin.h@gmail.com>
2024-03-07 22:41:00 +0100

    a

--- functions.c
+++ functions.c
@@ -110,7 +110,6 @@ void shuffle(char arr[], int n) {
 
 int get_line(char filename[], long focus, char **line, size_t *start) { // Making this function was hell. Hardest thing ive coded in a while.
     
-
     FILE *file;
     file = fopen(filename,"r"); // Open file
 
