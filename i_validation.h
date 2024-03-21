713f7440b97c70357bb507458305cb05e312f1f6
Oskar <assawin.h@gmail.com>
2024-03-21 11:55:43 +0100

    added i_validation.c and i_validation.h to make the code a bit easier and readable. Its my preparation for when i start dividing the validation functions in to smaller pieces

---
+++ i_validation.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include "7ed.h"
+#include <stdint.h>
+
+int validate_L(char *smode_buf);
+
+int validate_N(char *smode_buf);
